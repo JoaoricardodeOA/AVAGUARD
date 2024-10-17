7396ad5a6d050ce204c24691106d4b98a42db1e6
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2024-10-16 21:33:25 -0300

    Added first art

--- app/page.tsx
+++ app/page.tsx
@@ -9,26 +9,17 @@ import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import styles from '@/styles/homepage.module.css';
 
-const heroSectionStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  padding: 0,
-  gap: '27.84px',
-  position: 'relative', // Changed from absolute to relative for proper layout
-  width: '100%', // Change to relative width
-  height: 'auto', // Adjust based on content
-};
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-start p-0 gap-7">
-    <div className="flex flex-row justify-between items-center pt-16 pb-16 ps-28 pe-28 gap-20 ">
+    <div className="flex flex-col items-start p-0 gap-7 w-full bg-blue-100">
+    <div className="flex flex-row justify-between items-center pt-16 pb-16 ps-28 pe-28 gap-20 w-full">
       <div className="flex flex-col items-start gap-3">
-        <h1>Trazendo para as mulheres de sua empresa uma Proteção discreta, justiça segura.</h1>
-        <p>Respeito começa com coragem. Nós te apoiamos.</p>
+        <h1 className="text-large">Trazendo para as mulheres de sua empresa uma</h1>
+        <h1 className="text-large text-blue-800">Proteção discreta, justiça segura.</h1>
+        <p className="text-small">Respeito começa com coragem. Nós te apoiamos.</p>
       </div>
-      <div className={styles.heroIllustration}>
+      <div className="w-72">
       <Image
           src="/static/people.png"
           alt="Illustration"
