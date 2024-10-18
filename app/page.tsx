e2c4e1f68b750668bd3abfcb8dd7515712c44909
João Ricardo de Oliveira Agostinho <83128494+JoaoricardodeOA@users.noreply.github.com>
2024-10-17 22:02:52 -0300

    Added some changes on first page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-//"use client";
+"use client";
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
+import { Card,CardHeader,CardBody } from "@nextui-org/react";
+import { Button } from "@nextui-org/button";
 import Image from "next/image";
 //import { Card, Text } from '@nextui-org/react';
 
@@ -27,9 +29,9 @@ export default function Home() {
   return (
     <div className="flex flex-col"><div className="flex flex-col items-start p-0 gap-7 w-full bg-blue-100">
       <div className="flex flex-row justify-between items-center pt-16 pb-16 ps-28 pe-28 gap-20 w-full">
-        <div className="flex flex-col items-start gap-3">
-          <h1 className="text-large">Trazendo para as mulheres de sua empresa uma</h1>
-          <h1 className="text-large text-blue-800">Proteção discreta, justiça segura.</h1>
+        <div className="flex flex-col items-start gap-3 w-4/6">
+          <h1  style={{ fontSize: '2rem'}}>Trazendo para as mulheres de sua empresa uma</h1>
+          <h1 className=" text-blue-800" style={{ fontSize: '2rem'}}>Proteção discreta, justiça segura.</h1>
           <p className="text-small">Respeito começa com coragem. Nós te apoiamos.</p>
         </div>
         <div className="w-72">
@@ -44,7 +46,8 @@ export default function Home() {
     </div>
     <div className="flex flex-col items-center mt-3">
         <div
-          className="font-semibold text-center text-large"
+          className="font-semibold text-center"
+          style={{ fontSize: '1.5rem', fontWeight: 'bold' }}
         >
           Nossos Clientes
         </div>
@@ -64,6 +67,119 @@ export default function Home() {
           />
           ))}
         </div>
-      </div></div>
+      </div>
+      <div className="flex flex-row mt-5 ms-5 pt-5">
+      <Image
+            src='/static/1photo.png'
+            alt='photo'
+            width='307'
+            height='301'            
+          />
+      <div className="w-96">
+      <div className="pt-5">
+        <div className="">
+          <h1 className="text-large" style={{ fontWeight: 'bold' }}>Provas e Ferramentas Para Realizar Denúncias</h1>
+          <p className="pt-3">
+          Nossa principal missão é ajudar as vítimas a reunir provas contra os agressores de forma segura e eficaz. 
+          Oferecemos soluções inovadoras para capturar evidências discretamente, garantindo proteção e privacidade.
+          </p>
+        </div>
+        <Button color="primary" className="mt-3">
+                  Descubra
+                </Button>
+      </div>
+    </div>
+    
+    </div>
+
+    <div className="w-full flex flex-row ms-96">
+    <div className="flex flex-row mt-5 ms-32 pt-5">
+      <Image
+            src='/static/2photo.png'
+            alt='photo'
+            width='307'
+            height='301'            
+          />
+      <div className="w-96">
+      <div className="pt-5">
+        <div className="">
+          <h1 className="text-large " style={{ fontWeight: 'bold' }}>Criação de Ambientes de Trabalho Mais Seguros</h1>
+          <p className="pt-3">
+          Trabalhamos diretamente com empresas para desenvolver políticas e ambientes que promovam o respeito e 
+          combatam qualquer forma de assédio, ajudando a construir uma cultura corporativa saudável e ética.
+          </p>
+        </div>
+        <Button color="primary" className="mt-3">
+                  Descubra
+                </Button>
+      </div>
+    </div>
+    
+    </div>
+    </div>
+    <div className="flex flex-row mt-5 ms-5 pt-5">
+      <Image
+            src='/static/1photo.png'
+            alt='photo'
+            width='307'
+            height='301'            
+          />
+      <div className="w-96">
+      <div className="pt-5">
+        <div className="">
+          <h1 className="text-large" style={{ fontWeight: 'bold' }}>Assistência Jurídica Completa</h1>
+          <p className="pt-3">
+          Fornecemos orientação jurídica detalhada para garantir que cada vítima conheça seus direitos. 
+          Nossos advogados estão preparados para oferecer suporte em todas as etapas do processo legal.
+          </p>
+        </div>
+        <Button color="primary" className="mt-3">
+                  Descubra
+                </Button>
+      </div>
+    </div>
+    
+    </div>
+
+    <div className="w-full flex flex-row ms-96">
+    <div className="flex flex-row mt-5 ms-32 pt-5">
+      <Image
+            src='/static/2photo.png'
+            alt='photo'
+            width='307'
+            height='301'            
+          />
+      <div className="w-96">
+      <div className="pt-5">
+        <div className="">
+          <h1 className="text-large " style={{ fontWeight: 'bold' }}>Apoio Psicológico Personalizado</h1>
+          <p className="pt-3">
+          Oferecemos suporte emocional especializado para mulheres que sofreram assédio no ambiente de trabalho. 
+          Nossos psicólogos experientes estão prontos para ajudar na recuperação e fortalecimento da autoestima.
+          </p>
+        </div>
+        <Button color="primary" className="mt-3">
+                  Descubra
+                </Button>
+      </div>
+    </div>
+    
+    </div>
+    </div>
+    
+
+    <div className="w-full flex justify-center">
+    <Card className="py-4 mt-5 w-96">
+      <CardHeader className="pb-0 pt-2 px-4 flex-col text-center">
+        <h4 className="font-bold text-large">Frontend Radio</h4>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        
+      </CardBody>
+    </Card>
+    </div>
+    
+    </div>
+
   );
 }
