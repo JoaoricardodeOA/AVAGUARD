4bdf7318e3777149e1bbe8676de7b4706ea86a0c
José Emanuel da Silva Souza Albuquerque <emanuel.albuquerque@tatodesk.com.br>
2024-11-25 20:39:40 -0300

    ajustando o next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+//   publicRuntimeConfig: {
+//     NEXT_PUBLIC_AVAGUARD_API_URL: 'https://avaguard-api.vercel.app'
+//   }
+// };
+
 const nextConfig = {
   reactStrictMode: false,
   publicRuntimeConfig: {
-    NEXT_PUBLIC_AVAGUARD_API_URL: 'https://avaguard-api.vercel.app'
+    NEXT_PUBLIC_AVAGUARD_API_URL: 'http://localhost:3500'
   }
 };
 
