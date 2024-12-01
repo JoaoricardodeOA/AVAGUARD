e9ea26d1f49f950467f0902ed5c3a5bd2b6b2b55
José Emanuel da Silva Souza Albuquerque <emanuel.albuquerque@tatodesk.com.br>
2024-12-01 20:03:01 -0300

    Mudando a url

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: false,
-//   publicRuntimeConfig: {
-//     NEXT_PUBLIC_AVAGUARD_API_URL: 'https://avaguard-api-dgk8.vercel.app'
-//   }
-// };
-
 const nextConfig = {
   reactStrictMode: false,
   publicRuntimeConfig: {
-    NEXT_PUBLIC_AVAGUARD_API_URL: 'http://localhost:3500'
+    NEXT_PUBLIC_AVAGUARD_API_URL: 'https://avaguard-api-dgk8.vercel.app'
   }
 };
+
+// const nextConfig = {
+//   reactStrictMode: false,
+//   publicRuntimeConfig: {
+//     NEXT_PUBLIC_AVAGUARD_API_URL: 'http://localhost:3500'
+//   }
+// };
 
 export default nextConfig;
